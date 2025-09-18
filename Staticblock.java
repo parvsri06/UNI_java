18bf24491b027251ebebd0eac3347e35a0cea63c
parvsri06 <parv.srivastava.cs28@iilm.edu>
2025-09-18 09:50:47 +0530

    Create Staticblock.java

---
+++ Staticblock.java
@@ -0,0 +1,10 @@
+class main
+{
+    //static block
+    static {
+        system.out.println("Static block initialized. ");
+    }
+    public static void main(String[] args){
+        system.out.println("from main")
+    }
+}
